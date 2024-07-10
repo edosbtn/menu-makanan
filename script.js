ac77f9e9de5feeed0a128f27aebb85955a78a904
edosbtn <edoprabowo0903@gmail.com>
2024-07-11 05:31:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 const menuItems = [
-    { name: "Nasi Goreng", price: "Rp. 20,000" },
-    { name: "Mie Goreng", price: "Rp. 18,000" },
-    { name: "Sate Ayam", price: "Rp. 25,000" },
-    { name: "Gado-Gado", price: "Rp. 15,000" }
+    { name: "PAHA AYAM KAMPUNG ASLI", price: "Rp. 25.000", img: "paha-ayam-kampung.jpg" },
+    { name: "DADA AYAM KAMPUNG ASLI", price: "Rp. 25.000", img: "dada-ayam-kampung.jpg" },
+    { name: "PAHA BEBEK", price: "Rp. 25,000", img: "dada-bebek.jpg" },
+    { name: "DADA BEBEK", price: "Rp. 25,000", img: "paha-bebek.jpg" },
+    { name: "LELE (isi 2)", price: "Rp. 12,000", img: "lele.jpg" },
+    { name: "PUYUH", price: "Rp. 13,000", img: "puyuh.jpg" },
+    { name: "KEPALA AYAM KAMPUNG (isi 2)", price: "Rp. 12,000", img: "kepala-ayam-kampung.jpg" },
+    { name: "JEROAN (2 pasang)", price: "Rp. 13,000", img: "jeroan-rempela-ati.jpg" },
+    { name: "BELUT", price: "Rp. 12,000", img: "jeroan-rempela-ati.jpg" },    
+    { name: "KEPALA BEBEK (isi 2)", price: "Rp. 18,000", img: "jeroan-rempela-ati.jpg" },
+    { name: "TEMPE PENYET", price: "Rp. 6,000", img: "jeroan-rempela-ati.jpg" },
+    { name: "TEMPE TAHU PENYET", price: "Rp. 6,000", img: "jeroan-rempela-ati.jpg" },
+    { name: "SAMBAL TERONG", price: "Rp. 6,000", img: "jeroan-rempela-ati.jpg" }
+
 ];
 
 const menuList = document.getElementById('menu');
 
 menuItems.forEach(item => {
     const li = document.createElement('li');
-    li.innerHTML = `${item.name} <span>${item.price}</span>`;
+
+    const img = document.createElement('img');
+    img.src = item.img;
+    img.alt = item.name;
+
+    const div = document.createElement('div');
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = item.name;
+    const priceSpan = document.createElement('span');
+    priceSpan.textContent = item.price;
+    priceSpan.classList.add('price');
+
+    div.appendChild(nameSpan);
+    div.appendChild(priceSpan);
+
+    li.appendChild(img);
+    li.appendChild(div);
     menuList.appendChild(li);
 });
